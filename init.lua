c452a28bdacb8aa00fa1c793436d0be6ae18c9b4
Sam Bosley <sboz88@gmail.com>
2017-10-16 12:08:38 -0600

    Start cleaning up init.lua

--- init.lua
+++ init.lua
@@ -1,173 +1,164 @@
 hs.window.animationDuration = 0
 
 function adjustWindowFrame(transform)
-    if transform == nil then return end
+  if transform == nil then return end
 
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+  local win = hs.window.focusedWindow():frame()
+  local screen = win:screen():frame()
 
-    transform(f, max)
-    win:setFrame(f)
+  transform(f, screen)
+  win:setFrame(f)
+end
+
+function fillFrame(post)
+  adjustWindowFrame(function(win, screen)
+    win.x = screen.x
+    win.y = screen.y
+    win.w = screen.w
+    win.h = screen.h
+  end)
+  if post != nil then post(win, screen) end
 end
 
 pushModifiers = {"ctrl", "cmd"}
 
 hs.hotkey.bind(pushModifiers, "L", function()
-    adjustWindowFrame(function (f, max)
-        f.x = max.x + (max.w / 2)
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h
-    end)
+  fillFrame(function(win, screen)
+    win.x = screen.x + (screen.w / 2)
+    win.w = screen.w / 2
+  end)
 end)
 
 hs.hotkey.bind(pushModifiers, "J", function()
-    adjustWindowFrame(function (f, max)
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h
-    end)
+  fillFrame(function(win, screen)
+    win.w = screen.w / 2
+  end)
 end)
 
 hs.hotkey.bind(pushModifiers, "I", function()
-    adjustWindowFrame(function (f, max)
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w
-        f.h = max.h / 2
-    end)
+  fillFrame(function (win, screen)
+    win.h = screen.h / 2
+  end)
 end)
 
 hs.hotkey.bind(pushModifiers, ",", function()
-    adjustWindowFrame(function (f, max)
-        f.x = max.x
-        f.y = max.y + (max.h / 2)
-        f.w = max.w
-        f.h = max.h / 2
-    end)
+  fillFrame(function (win, screen)
+    win.y = screen.y + (screen.h / 2)
+    win.h = screen.h / 2
+  end)
 end)
 
 moveModifiers = {"shift", "alt"}
 movePercentage = 5
 moveDivisor = 100 / movePercentage
-function xNudgeAmount(max)
-    return max.w / moveDivisor
+function xNudgeAmount(screen)
+  return screen.w / moveDivisor
 end
 
-function yNudgeAmount(max)
-    return max.h / moveDivisor
+function yNudgeAmount(screen)
+  return screen.h / moveDivisor
 end
 
 hs.hotkey.bind(moveModifiers, "L", function()
-    adjustWindowFrame(function (f, max)
-        local newX = f.x + xNudgeAmount(max)
-        if newX + f.w > max.x + max.w then
-            newX = max.x + max.w - f.w
-        end
-        f.x = newX
-    end)
+  adjustWindowFrame(function (win, screen)
+    local newX = win.x + xNudgeAmount(screen)
+    if newX + win.w > screen.x + screen.w then
+      newX = screen.x + screen.w - win.w
+    end
+    win.x = newX
+  end)
 end)
 
 hs.hotkey.bind(moveModifiers, "J", function()
-    adjustWindowFrame(function (f, max)
-        local newX = f.x - xNudgeAmount(max)
-        if newX < max.x then
-            newX = max.x
-        end
-        f.x = newX
-    end)
+  adjustWindowFrame(function (win, screen)
+    local newX = win.x - xNudgeAmount(screen)
+    if newX < screen.x then
+      newX = screen.x
+    end
+    win.x = newX
+  end)
 end)
 
 hs.hotkey.bind(moveModifiers, "I", function()
-    adjustWindowFrame(function (f, max)
-        local newY = f.y - yNudgeAmount(max)
-        if newY < max.y then
-            newY = max.y
-        end
-        f.y = newY
-    end)
+  adjustWindowFrame(function (win, screen)
+    local newY = win.y - yNudgeAmount(screen)
+    if newY < screen.y then
+      newY = screen.y
+    end
+    win.y = newY
+  end)
 end)
 
 hs.hotkey.bind(moveModifiers, ",", function()
-    adjustWindowFrame(function (f, max)
-        local newY = f.y + yNudgeAmount(max)
-        if newY + f.y > max.y + max.h then
-            newY = max.y + max.h - f.h
-        end
-        f.y = newY
-    end)
+  adjustWindowFrame(function (win, screen)
+    local newY = win.y + yNudgeAmount(screen)
+    if newY + win.y > screen.y + screen.h then
+      newY = screen.y + screen.h - win.h
+    end
+    win.y = newY
+  end)
 end)
 
 resizePositiveModifiers = {"cmd", "alt"}
 resizePercentage = 5
 resizeDivisor = 100 / resizePercentage
 
-function xResizeAmount(max)
-    return max.w / resizeDivisor
+function xResizeAmount(screen)
+  return screen.w / resizeDivisor
 end
 
-function yResizeAmount(max)
-    return max.h / resizeDivisor
+function yResizeAmount(screen)
+  return screen.h / resizeDivisor
 end
 
 hs.hotkey.bind(resizePositiveModifiers, "L", function()
-    adjustWindowFrame(function(f, max)
-        local newW = f.w + xResizeAmount(max)
-        if f.x + newW > max.x + max.w then
-            newW = max.x + max.w - f.x
-        end
-        f.w = newW
-    end)
+  adjustWindowFrame(function(win, screen)
+    local newW = win.w + xResizeAmount(screen)
+    if win.x + newW > screen.x + screen.w then
+      newW = screen.x + screen.w - win.x
+    end
+    win.w = newW
+  end)
 end)
 
 hs.hotkey.bind(resizePositiveModifiers, "J", function()
-    adjustWindowFrame(function(f, max)
-        local xResize = xResizeAmount(max)
-        local newW = f.w - xResize
-        if newW < xResize then
-            newW = xResize
-        end
-        f.w = newW
-    end)
+  adjustWindowFrame(function(win, screen)
+    local xResize = xResizeAmount(screen)
+    local newW = win.w - xResize
+    if newW < xResize then
+      newW = xResize
+    end
+    win.w = newW
+  end)
 end)
 
 hs.hotkey.bind(resizePositiveModifiers, "I", function()
-    adjustWindowFrame(function(f, max)
-        local yResize = yResizeAmount(max)
-        local newH = f.h - yResize
-        if newH < yResize then
-            newH = yResize
-        end
-        f.h = newH
-
-
-    end)
+  adjustWindowFrame(function(win, screen)
+    local yResize = yResizeAmount(screen)
+    local newH = win.h - yResize
+    if newH < yResize then
+      newH = yResize
+    end
+    win.h = newH
+  end)
 end)
 
 hs.hotkey.bind(resizePositiveModifiers, ",", function()
-    adjustWindowFrame(function(f, max)
-        local newH = f.h + yResizeAmount(max)
-        if f.y + newH > max.y + max.h then
-            newH = max.y + max.h - f.y
-        end
-        f.h = newH
-    end)
+  adjustWindowFrame(function(win, screen)
+    local newH = win.h + yResizeAmount(screen)
+    if win.y + newH > screen.y + screen.h then
+      newH = screen.y + screen.h - win.y
+    end
+    win.h = newH
+  end)
 end)
 
 function throw(screenNumber)
-    local screens = hs.screen.allScreens()
-    if screenNumber > #screens then return end
+  local screens = hs.screen.allScreens()
+  if screenNumber > #screens then return end
 
-    hs.window.focusedWindow():moveToScreen(screens[screenNumber])
-    adjustWindowFrame(function(f, max)
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w
-        f.h = max.h
-    end)
+  hs.window.focusedWindow():moveToScreen(screens[screenNumber])
+  fillFrame(nil)
 end
 
 throwModifiers = {"cmd", "alt"}
