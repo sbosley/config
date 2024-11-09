3c127cdfcf25517b03014ec435b08182c46abdab
Sam Bosley <sboz88@gmail.com>
2024-11-09 14:10:05 -0700

    Minor cleanup to init.lua

--- init.lua
+++ init.lua
@@ -1,68 +1,78 @@
 hs.window.animationDuration = 0
+log = hs.logger.new("config")
+
+roon = require("roon")
 
 function pushWindow(rect)
-  hs.window.focusedWindow():moveToUnit(rect)
+  return function() hs.window.focusedWindow():moveToUnit(rect) end
 end
 
 function moveWindow(delta)
-  hs.window.focusedWindow():move(delta, nil, true)
+  return function() hs.window.focusedWindow():move(delta, nil, true) end
 end
 
 function resizeWindow(delta)
-  local win = hs.window.focusedWindow()
-  local size = win:size()
-  size.w = size.w + delta.x
-  size.h = size.h + delta.y
-  win:setSize(size)
-end
-
-function throwWindow(screenNumber)
-  local screens = hs.screen.allScreens()
-  if screenNumber > #screens then return end
-
-  hs.window.focusedWindow():moveToScreen(screens[screenNumber])
-  pushWindow('[0,0 100x100]')
-end
-
-function increment(dir)
-  local screen = hs.window.focusedWindow():screen():frame()
-  if dir == "x" then
-    return screen.w * 5 / 100
-  else
-    return screen.h * 5 / 100
+  return function()
+    local win = hs.window.focusedWindow()
+    local size = win:size()
+    size.w = size.w + delta.x
+    size.h = size.h + delta.y
+    win:setSize(size)
   end
 end
 
-function plusX() return hs.geometry(increment("x"), 0) end
-function minusX() return hs.geometry(-1*increment("x"), 0) end
-function plusY() return hs.geometry(0, increment("y")) end
-function minusY() return hs.geometry(0, -1*increment("y")) end
+function throwWindow(screenNumber)
+  return function()
+    local screens = hs.screen.allScreens()
+    if screenNumber > #screens then return end
+
+    hs.window.focusedWindow():moveToScreen(screens[screenNumber])
+    pushWindow('[0,0 100x100]')()
+  end
+end
+
+function screenFrame() return hs.window.focusedWindow():screen():frame() end
+function incrX() return hs.geometry(screenFrame().w * 5 / 100, 0) end
+function decrX() return hs.geometry(screenFrame().w * -5 / 100, 0) end
+function incrY() return hs.geometry(0, screenFrame().h * 5 / 100) end
+function decrY() return hs.geometry(0, screenFrame().h * -5 / 100) end
+
+function changeRoonVolume(delta)
+  return function() roon.changeVolume(delta) end
+end
 
 pushModifiers = {"ctrl", "cmd"}
-hs.hotkey.bind(pushModifiers, "L", function() pushWindow('[50,0 50x100]') end)
-hs.hotkey.bind(pushModifiers, "J", function() pushWindow('[0,0 50x100]') end)
-hs.hotkey.bind(pushModifiers, "I", function() pushWindow('[0,0 100x50]') end)
-hs.hotkey.bind(pushModifiers, ",", function() pushWindow('[0,50 100x50]') end)
-hs.hotkey.bind(pushModifiers, "U", function() pushWindow('[0,0 50x50]') end)
-hs.hotkey.bind(pushModifiers, "O", function() pushWindow('[50,0 50x50]') end)
-hs.hotkey.bind(pushModifiers, "M", function() pushWindow('[0,50 50x50]') end)
-hs.hotkey.bind(pushModifiers, ".", function() pushWindow('[50,50 50x50]') end)
-hs.hotkey.bind(pushModifiers, "K", function() pushWindow('[0,0 100x100]') end)
+hs.hotkey.bind(pushModifiers, "L", pushWindow('[50,0 50x100]'))
+hs.hotkey.bind(pushModifiers, "J", pushWindow('[0,0 50x100]'))
+hs.hotkey.bind(pushModifiers, "I", pushWindow('[0,0 100x50]'))
+hs.hotkey.bind(pushModifiers, ",", pushWindow('[0,50 100x50]'))
+hs.hotkey.bind(pushModifiers, "U", pushWindow('[0,0 50x50]'))
+hs.hotkey.bind(pushModifiers, "O", pushWindow('[50,0 50x50]'))
+hs.hotkey.bind(pushModifiers, "M", pushWindow('[0,50 50x50]'))
+hs.hotkey.bind(pushModifiers, ".", pushWindow('[50,50 50x50]'))
+hs.hotkey.bind(pushModifiers, "K", pushWindow('[0,0 100x100]'))
 
 moveModifiers = {"shift", "alt"}
-hs.hotkey.bind(moveModifiers, "L", function() moveWindow(plusX()) end)
-hs.hotkey.bind(moveModifiers, ",", function() moveWindow(plusY()) end)
-hs.hotkey.bind(moveModifiers, "J", function() moveWindow(minusX()) end)
-hs.hotkey.bind(moveModifiers, "I", function() moveWindow(minusY()) end)
+hs.hotkey.bind(moveModifiers, "L", moveWindow(incrX()))
+hs.hotkey.bind(moveModifiers, ",", moveWindow(incrY()))
+hs.hotkey.bind(moveModifiers, "J", moveWindow(decrX()))
+hs.hotkey.bind(moveModifiers, "I", moveWindow(decrY()))
 
 resizeModifiers = {"cmd", "alt"}
-hs.hotkey.bind(resizeModifiers, "L", function() resizeWindow(plusX()) end)
-hs.hotkey.bind(resizeModifiers, ",", function() resizeWindow(plusY()) end)
-hs.hotkey.bind(resizeModifiers, "J", function() resizeWindow(minusX()) end)
-hs.hotkey.bind(resizeModifiers, "I", function() resizeWindow(minusY()) end)
+hs.hotkey.bind(resizeModifiers, "L", resizeWindow(incrX()))
+hs.hotkey.bind(resizeModifiers, ",", resizeWindow(incrY()))
+hs.hotkey.bind(resizeModifiers, "J", resizeWindow(decrX()))
+hs.hotkey.bind(resizeModifiers, "I", resizeWindow(decrY()))
 
 throwModifiers = {"cmd", "alt"}
-hs.hotkey.bind(throwModifiers, "1", function() throw(1) end)
-hs.hotkey.bind(throwModifiers, "2", function() throw(2) end)
-hs.hotkey.bind(throwModifiers, "3", function() throw(3) end)
-hs.hotkey.bind(throwModifiers, "4", function() hs.application.find("Roon"):activate() end)
+hs.hotkey.bind(throwModifiers, "1", throwWindow(1))
+hs.hotkey.bind(throwModifiers, "2", throwWindow(2))
+hs.hotkey.bind(throwModifiers, "3", throwWindow(3))
+
+volumeModifiers = {"cmd"}
+hs.hotkey.bind(volumeModifiers, "f11", changeRoonVolume(-2))
+hs.hotkey.bind(volumeModifiers, "f12", changeRoonVolume(2))
+
+bigVolumeModifiers = {"cmd", "alt", "ctrl"}
+hs.hotkey.bind(bigVolumeModifiers, "f11", changeRoonVolume(-4))
+hs.hotkey.bind(bigVolumeModifiers, "f12", changeRoonVolume(4))
