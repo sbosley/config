c948b64a655d8ff47449b24ea7cecbe52f9e9e58
Sam Bosley <sboz88@gmail.com>
2024-12-23 18:46:35 -0700

    Make init.lua work better with multiple screens of different dimensions

--- init.lua
+++ init.lua
@@ -8,16 +8,27 @@ function pushWindow(rect)
 end
 
 function moveWindow(delta)
-  return function() hs.window.focusedWindow():move(delta, nil, true) end
+  return function()
+    local win = hs.window.focusedWindow()
+    win:move(delta(win), nil, true)
+  end
 end
 
 function resizeWindow(delta)
   return function()
     local win = hs.window.focusedWindow()
     local size = win:size()
-    size.w = size.w + delta.x
-    size.h = size.h + delta.y
+    local d = delta(win)
+    size.w = size.w + d.x
+    size.h = size.h + d.y
     win:setSize(size)
+  end
+end
+
+function changeScreen(screenFunc)
+  return function()
+    local win = hs.window.focusedWindow()
+    win[screenFunc](win)
   end
 end
 
@@ -31,11 +42,19 @@ function throwWindow(screenNumber)
   end
 end
 
-function screenFrame() return hs.window.focusedWindow():screen():frame() end
-function incrX() return hs.geometry(screenFrame().w * 5 / 100, 0) end
-function decrX() return hs.geometry(screenFrame().w * -5 / 100, 0) end
-function incrY() return hs.geometry(0, screenFrame().h * 5 / 100) end
-function decrY() return hs.geometry(0, screenFrame().h * -5 / 100) end
+function incr(dim) return dim * 5 / 100 end
+
+function deltaX(mult)
+  return function(win)
+    return hs.geometry(incr(win:screen():frame().w * mult), 0)
+  end
+end
+
+function deltaY(mult)
+  return function(win)
+    return hs.geometry(0, incr(win:screen():frame().h * mult))
+  end
+end
 
 function changeRoonVolume(delta)
   return function() roon.changeVolume(delta) end
@@ -51,18 +70,20 @@ hs.hotkey.bind(pushModifiers, "O", pushWindow('[50,0 50x50]'))
 hs.hotkey.bind(pushModifiers, "M", pushWindow('[0,50 50x50]'))
 hs.hotkey.bind(pushModifiers, ".", pushWindow('[50,50 50x50]'))
 hs.hotkey.bind(pushModifiers, "K", pushWindow('[0,0 100x100]'))
+hs.hotkey.bind(pushModifiers, "left", changeScreen('moveOneScreenWest'))
+hs.hotkey.bind(pushModifiers, "right", changeScreen('moveOneScreenEast'))
 
 moveModifiers = {"shift", "alt"}
-hs.hotkey.bind(moveModifiers, "L", moveWindow(incrX()))
-hs.hotkey.bind(moveModifiers, ",", moveWindow(incrY()))
-hs.hotkey.bind(moveModifiers, "J", moveWindow(decrX()))
-hs.hotkey.bind(moveModifiers, "I", moveWindow(decrY()))
+hs.hotkey.bind(moveModifiers, "L", moveWindow(deltaX(1)))
+hs.hotkey.bind(moveModifiers, ",", moveWindow(deltaY(1)))
+hs.hotkey.bind(moveModifiers, "J", moveWindow(deltaX(-1)))
+hs.hotkey.bind(moveModifiers, "I", moveWindow(deltaY(-1)))
 
 resizeModifiers = {"cmd", "alt"}
-hs.hotkey.bind(resizeModifiers, "L", resizeWindow(incrX()))
-hs.hotkey.bind(resizeModifiers, ",", resizeWindow(incrY()))
-hs.hotkey.bind(resizeModifiers, "J", resizeWindow(decrX()))
-hs.hotkey.bind(resizeModifiers, "I", resizeWindow(decrY()))
+hs.hotkey.bind(resizeModifiers, "L", resizeWindow(deltaX(1)))
+hs.hotkey.bind(resizeModifiers, ",", resizeWindow(deltaY(1)))
+hs.hotkey.bind(resizeModifiers, "J", resizeWindow(deltaX(-1)))
+hs.hotkey.bind(resizeModifiers, "I", resizeWindow(deltaY(-1)))
 
 throwModifiers = {"cmd", "alt"}
 hs.hotkey.bind(throwModifiers, "1", throwWindow(1))
